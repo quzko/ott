53b740e7f8ce7814c24d457438bffe5b86f55be6
Пелевина Ирина Олеговна <IOPelevina.OUT@sberbank.ru>
2018-12-13 15:21:35 +0300

    правки

--- src/test/java/ru/ott/autotest/SuggestRequest.java
+++ src/test/java/ru/ott/autotest/SuggestRequest.java
@@ -1,15 +1,16 @@
 package ru.ott.autotest;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import io.restassured.RestAssured;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import ru.ott.autotest.model.ParametersList;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class SuggestRequest {
+    private static String param = "&lang=ru&locale=ru&currency=RUB";
+    private static ParametersList list = new ParametersList();
+
     @BeforeMethod
     public void setUp() {
         RestAssured.baseURI = "https://www.onetwotrip.com/_hotels/api/suggestRequest";
@@ -18,10 +19,8 @@ public class SuggestRequest {
     @Test(description = "Проверка положительного ответа", priority = 1)
     public void responseTest() {
         given()
-                .queryParam("query", "Мос")
-                .queryParam("lang", "ru")
-                .queryParam("locale", "ru")
-                .queryParam("currency", "RUB").
+                .queryParam("query", list.getName())
+                .queryParam(param).
                 when()
                 .get()
                 .then().statusCode(200);
@@ -30,16 +29,14 @@ public class SuggestRequest {
     @Test(description = "Проверка наличия не пустых полей в ответе", priority = 2)
     public void restResultTest() {
         given()
-                .queryParam("query", "Мос")
-                .queryParam("lang", "ru")
-                .queryParam("locale", "ru")
-                .queryParam("currency", "RUB").
+                .queryParam("query", list.getName())
+                .queryParam(param).
                 when()
                 .get()
                 .then()
+                .log().all()
                 .body(matchesJsonSchemaInClasspath("schema.json"));
     }
-
 }
 
 
---
+++ src/test/java/ru/ott/autotest/model/ParametersList.java
@@ -0,0 +1,15 @@
+package ru.ott.autotest.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class ParametersList {
+    public String getName() {
+        List<String> list = Arrays.asList("Москва","Italy","Карелия","Африка","Сан-Фр","Китай","Radisson","Домодедово","SVO","123");
+        Random random = new Random();
+        String foo = list.get(random.nextInt(list.size()));
+        System.out.println(foo);
+        return foo;
+    }
+}
